d9d44ce0ca3e657b742fef8f88afa40b56c38caa
woshyy <chrwa634@student.liu.se>
2023-02-20 17:16:03 +0100

    update

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
 from dash import Dash, dcc, html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
+import dash
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Input(id='input-1-state', type='text', value='Montreal'),
-    dcc.Input(id='input-2-state', type='text', value='Canada'),
-    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
-    html.Div(id='number-output-state')
+    html.P('Enter a composite number to see its prime factors'),
+    dcc.Input(id='num', type='number', debounce=True, min=2, step=1),
+    html.P(id='err', style={'color': 'red'}),
+    html.P(id='out')
 ])
 
-
 @app.callback(
-    Output('number-output-state', 'children'),
-    Input('submit-button-state', 'n_clicks'),
-    State('input-1-state', 'value'),
-    State('input-2-state', 'value'))
-def update_output(n_clicks, input1, input2):
-    return u'''
-        The button has been pressed {} times,
-        Input 1 is "{}",
-        and Input 2 is "{}"
-    '''.format(n_clicks, input1, input2)
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    Output('out', 'children'),
+    Output('err', 'children'),
+    Input('num', 'value'))
+def show_factors(num):
+    if num is None:
+        raise dash.exceptions.PreventUpdate
+    
+    factors = prime_factors(num)
